1a7e2790afe615e0cd9396288ca41489cf162b68
Emory Merryman <emory.merryman@gmail.com>
2023-01-29 18:51:53 -0500

    one happy test

--- original.nix
+++ original.nix
@@ -3,18 +3,15 @@
       {
         flake-utils.url = "github:numtide/flake-utils?rev=5aed5285a952e0b949eb3ba02c12fa4fcfef535f" ;
 	implementation.url = "${IMPLEMENTATION}" ;
-	nixpkgs.url = "github:nixos/nixpkgs?rev=57eac89459226f3ec743ffa6bbbc1042f5836843" ;
 	test.url = "${TEST}" ;
-	tester.url = "${TESTER}" ;
+	tester.url = "github:nextmoose/tester" ;
       } ;
     outputs =
-      { flake-utils , implementation , nixpkgs , self , test , tester } :
+      { flake-utils , implementation , self , test } :
         flake-utils.lib.eachDefaultSystem
 	  (
 	    system :
 	      let
-	        pkgs = builtins.getAttr system nixpkgs.legacyPackages ;
-		# in { devShell = pkgs.mkShell { buildInputs = [ ( pkgs.writeShellScriptBin "check" "${ pkgs.coreutils }/bin/echo FOUND" ) ] ; } ; }
 		in builtins.getAttr system tester.lib implementation test
 	  ) ;
   }
