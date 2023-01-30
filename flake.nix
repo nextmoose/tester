11c1ac6a1f40c5735cab64a603c9ca07c6dcb13e
Emory Merryman <emory.merryman@gmail.com>
2023-01-29 19:58:23 -0500

    do not use local

--- flake.nix
+++ flake.nix
@@ -3,7 +3,7 @@
       {
         flake-utils.url = "github:numtide/flake-utils?rev=5aed5285a952e0b949eb3ba02c12fa4fcfef535f" ;
         nixpkgs.url = "github:nixos/nixpkgs?rev=57eac89459226f3ec743ffa6bbbc1042f5836843" ;
-        utils.url = "/home/emory/projects/MGWfXwul" ;
+        utils.url = "github:nextmoose/utils" ;
       } ;
     outputs =
       { flake-utils , nixpkgs , self , utils } :
