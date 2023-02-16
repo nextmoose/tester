a1e8de578b3d3170e56594cca2555b95144a1ce4
Emory Merryman <emory.merryman@gmail.com>
2023-02-16 09:46:33 -0500

    yes

---
+++ yaml/jq.sh
@@ -0,0 +1,3 @@
+#!bin/sh
+
+cat flake.lock | jq --raw-output '.root as $root | .nodes | to_entries | map ( select ( .key != $root ) ) | map ( { key : .key , value : .value.original } ) | map ( ( .value.type == "github" and ( .value | has ( "owner" ) ) and ( .value | has ( "repo" ) ) and ( .value | has ( "rev" ) ) ) )'
