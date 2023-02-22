546d2daca243c9517ae80282e5758ed2a2662919
Emory Merryman <emory.merryman@gmail.com>
2023-02-21 22:17:21 -0500

    restart

--- shell.nix
+++ shell.nix
@@ -30,6 +30,7 @@
                           runs-on = "ubuntu-latest" ;
                           steps =
                             [
+                              { uses = "actions/checkout@v3" ; }
                               { uses = "cachix/install-nix-action@v17" ; "b200830c-8d41-4c5d-964c-5ecaaba35204" = { extra_nix_config = "access-tokens = github.com = ${ dollar "{ secrets.TOKEN }" }" ; } ; }
                               { run = "nix-shell .github/workflows/check/shell.nix --command check" ; }
                             ] ;
