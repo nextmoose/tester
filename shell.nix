b6049e76ef4e05e37d6279a5b758096c713e4bf8
Emory Merryman <emory.merryman@gmail.com>
2023-03-02 07:49:13 -0500

    try experiment

--- shell.nix
+++ shell.nix
@@ -3,56 +3,83 @@
     {
       buildInputs =
         let
+	  constants =
+	    {
+	      on = "b0980023-1d94-4330-9115-5a38a20088c5" ;
+	      push = "68eb0e41-0b86-40ea-a11f-85be4e43ac4d" ;
+	      _with = "c1f17318-5d0a-4afb-8c0e-1d046e834104" ;
+	    } ;
           dollar = expression : builtins.concatStringsSep "" [ "$" "{" ( builtins.toString expression ) "}" ] ;
 	  objects =
-	    let
-	      base =
-	        constants :
-		  {
-		    name = "test" ;
-		    "${ constants.on }" =
-		      {
-		        push = constants.push ;
-		      } ;
-		  } ;
-	      in
+	    {
+	      init =
 	        {
-	        } ;
-	  yq =
-	    root : expressions :
-	      let
-	        constants = inner-constants // outer-constants ;
-	        inner-constants =
-		  {
-		    delete = "67fe9630-a4ae-4a39-9dbf-a790a5575f1b" ;
-		    period = "6ff1b0c2-75bf-46cc-b0e9-7e75d687f8e4" ;
-		  } ;
-	        outer-constants =
-		  {
-		    on = "df5d8dd4-a49f-4c1f-b6e6-9ca45ee30494" ;
-		    push = "9ab16531-3d37-4c87-b293-cfc55370a934" ;
-		  } ;
-		worder = expression : expression constants ;
-		enquote = expression : builtins.concatStringsSep "" [ "\"" ( builtins.toString expression ) "\"" ] ;
-		decoder = builtins.replaceStrings [ ( enquote inner-constants.delete ) ( enquote inner-constants.period ) ] [ "del(.true)" "." ] ;
-		phrase = builtins.concatStringsSep " + " ( builtins.map decoder ( builtins.map builtins.toJSON ( builtins.map worder expressions ) ) ) ;
-	        program =
-		  pkgs.writeShellScript
-		    "yq"
-		    ''
-		      ${ if root then "" else "${ pkgs.coreutils }/bin/tee |" } ${ pkgs.yq }/bin/yq ${ if root then "-n" else "" } --yaml-output '${ phrase }' | ${ pkgs.gnused }/bin/sed -e "s#${ outer-constants.on }#on#" -e "s#${ outer-constants.push }##"
-		    '' ;
-		in program ;
+		  name = "test" ;
+		  "${ constants.on }" =
+		    {
+		      push = constants.on ;
+		    } ;
+		  env =
+		    {
+		      implementation-base = "${ dollar "IMPLEMENTATION_BASE" }" ;
+		      test-base = "${ dollar "TEST_BASE" }" ;
+		      tester-base = "${ dollar "TESTER_BASE" }" ;
+		    } ;
+		  jobs =
+		    {
+		      pre-check =
+		        {
+			} ;
+		      check =
+		        {
+		          runs-on = "ubuntu-latest" ;
+			  needs = [ "pre-check" ] ;
+			  steps =
+			    [
+			      { uses = "actions/checkout@v3" ; }
+			      { uses = "cachix/install-nix-action@v18" ; "${ constants._with }" = { extra_nix_config = "access-tokens = github.com = ${ "{ github.TOKEN }" }" } ; }
+			      {
+			        uses = "workflow/nix-shell-action@v3" ;
+				"${ constants._with }" =
+				  {
+				    flakes = "tester#check" ;
+				    script =
+				      ''
+				      '' ;
+				  } ;
+			      }
+			    ] ;
+			} ;
+		    } ;
+		} ;
+	    } ;
           in
             [
-	      (
-	        pkgs.writeShellScriptBin
-		  "mine"
-		  ''
-		    ${ yq true [ ( constants : constants.delete ) ( constants : { name = "feed" ; "${ constants.on }" = { push = constants.push ; } ; } ) ] }
-		  ''
-	      )
-	      pkgs.cowsay
+              (
+                pkgs.writeShellScriptBin
+                  "write-init"
+                  ''
+		    if [ -d .github ]
+		    then
+		      ${ pkgs.coreutils }/bin/mkdir .github
+		    fi &&
+		    if [ -d .github/workflows ]
+		    then
+		      ${ pkgs.coreutils }/bin/mkdir .github/workflows
+		    fi &&
+                    ${ pkgs.yq }/bin/yq -n --yaml-output '${ objects.init }' | ${ sed } > .github/workflows/test.yaml &&
+		    ${ pkgs.coreutils }/bin/chmod 0400 .github/workflows/test.yaml &&
+		    ${ pkgs.git }/bin/git add .github/workflows/test.yaml
+                  ''
+              )
+              (
+                pkgs.writeShellScriptBin
+                  "pigsay"
+                  ''
+                    ${ pkgs.gnused }/bin/sed -e "s#MESSAGE#${ dollar "@" }#" ${ ./pig.ascii }
+                  ''
+              )
+              pkgs.cowsay
               pkgs.chromium
               pkgs.coreutils
               pkgs.emacs
@@ -67,9 +94,6 @@
                 pkgs.writeShellScriptBin
                   "manual-check-test"
                   ''
-                    IMPLEMENTATION="${ dollar 1 }" &&
-                    TESTER="${ dollar 2 }" &&
-                    DEFECT="${ dollar 3 }" &&
                     ${ pkgs.nix }/bin/nix-shell \
                       .github/workflows/check/shell.nix \
                       --argstr implementation-base "${ dollar "IMPLEMENTATION" }" \
@@ -97,7 +121,7 @@
             ] ;
       shellHook =
         ''
-          export ARGUE_HOME=/home/emory/projects/h9QAx8XE &&
+          export ARGUE_HOME=/home/emory/projects/h9QAxXE &&
           export APPLY_HOME=/home/emory/projects/L5bpxC6n &&
           export BASH_VARIABLE_HOME=/home/emory/projects/5juNXfpb &&
           export PERSONAL_HOME=/home/emory/projects/71tspv3q &&
