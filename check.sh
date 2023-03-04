20af5a8166d484e45a30550bbf72799eeca99957
Emory Merryman <emory.merryman@gmail.com>
2023-03-04 16:58:11 -0500

    YES

--- check.sh
+++ check.sh
@@ -1,14 +1,6 @@
 #!/bin/sh
 
-export IMPLEMENTATION_URL="${1}" &&
-    export IMPLEMENTATION_POSTULATE="${2}" &&
-    export TEST_URL="${3}" &&
-    export TEST_POSTULATE="${4}" &&
-    export TEST_REV="${5}" &&
-    export DEFECT="${6}" &&
-    export POSTULATE="${7}" &&
-    export TOKEN="${8}" &&
-    env &&
-    echo BEFORE &&
-    curl -L https://nixos.org/nix/install | sh &&
-    echo AFTER
+echo BEFORE &&
+    sh ./install-nix.sh &&
+    echo AFTER &&
+    env
