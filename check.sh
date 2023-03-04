20bcebffaf23f135282706607f0203ac749db31b
Emory Merryman <emory.merryman@gmail.com>
2023-03-04 14:35:24 -0500

    YES

--- check.sh
+++ check.sh
@@ -1,6 +1,13 @@
 #!/bin/sh
 
 
-DEFECT=${1} &&
-    POSTULATE=${2} &&
-    echo ${DEFECT} ${POSTULATE}
+IMPLEMENTATION_URL=${1} &&
+    IMPLEMENTATION_POSTULATE=${2} &&
+    TEST_URL=${3} &&
+    TEST_POSTULATE=${4} &&
+    TEST_REV=${5} &&
+    DEFECT=${6} &&
+    POSTULATE=${7} &&
+    TOKEN=${8} &&
+    env &&
+    apt-get update
