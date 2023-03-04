5dbd4e9623681f2edacb05b195e9d2c7237af5c3
Emory Merryman <emory.merryman@gmail.com>
2023-03-04 15:14:10 -0500

    YES

--- check.sh
+++ check.sh
@@ -1,13 +1,12 @@
 #!/bin/sh
 
-
-IMPLEMENTATION_URL=${1} &&
-    IMPLEMENTATION_POSTULATE=${2} &&
-    TEST_URL=${3} &&
-    TEST_POSTULATE=${4} &&
-    TEST_REV=${5} &&
-    DEFECT=${6} &&
-    POSTULATE=${7} &&
-    TOKEN=${8} &&
+export IMPLEMENTATION_URL="${1}" &&
+    export IMPLEMENTATION_POSTULATE="${2}" &&
+    export TEST_URL="${3}" &&
+    export TEST_POSTULATE="${4}" &&
+    export TEST_REV="${5}" &&
+    export DEFECT="${6}" &&
+    export POSTULATE="${7}" &&
+    export TOKEN="${8}" &&
     env &&
     apt-get update
