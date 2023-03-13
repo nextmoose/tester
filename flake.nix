8588fd7dfe5c1f6b9df1fe8a2718bac077724b7c
Emory Merryman <emory.merryman@gmail.com>
2023-03-13 14:25:05 -0400

    fix

--- flake.nix
+++ flake.nix
@@ -17,39 +17,28 @@
 		  {
 		    lib =
 		      implementation : test :
-		        let
-			  _implementation =
-			    if builtins.typeOf implementation == "set" && builtins.hasAttr "lib" implementation && builtins.hasAttr system implementation.lib then builtins.getAttr system implementation.lib
-			    else builtins.throw "171cb779-5e01-40da-b52a-0cd26fc1975e" ;
-			  _test =
-			    if builtins.typeOf test == "set" && builtins.hasAttr "lib" test && builtins.hasAttr system test.lib then builtins.getAttr system test.lib
-			    else builtins.throw "08e93433-5518-45d7-9815-499f37b15654" ;
-			  _utils = builtins.getAttr system utils.lib ;
-			  check =
-			    let
-			      lambda =
-			        track :
-				  let
-				    test = observer : success : value :
-				      let
-				        expected = { success = success ; value = value ; } ;
-					observed = builtins.tryEval ( observer _implementation ) ;
-					in observed == expected ;
-				    in if track.reduced test then "" else stringify track.path ;
-			       list = track : builtins.concatStringsSep "," ( builtins.filter ( x : x != "" ) track.reduced ) ;
-			       set = track : builtins.concatStringsSep "," ( builtins.filter ( x : x != "" ) ( builtins.attrValues track.reduced ) ) ;
-			       undefined = track : builtins.throw "7f74b417-f07a-4d6a-8257-fdeb47ee89ae" ;
-			       in _utils.visit { lambda = lambda ; list = list ; set = set ; undefined = undefined ; } _test ;
-			  devShell = { devShell = pkgs.mkShell { buildInputs = [ ( pkgs.writeShellScriptBin "check" "${ pkgs.coreutils }/bin/echo ${ check }" ) ] ; } ; } ;
-			  error = builtins.throw "21ad4f91-60d5-4603-a721-2b0037abc004" ;
-			  stringify =
-			    let
-			      int = track : builtins.concatStringsSep "" [ "[" ( builtins.toString track.reduced ) "]" ] ;
-			      list = track : builtins.concatStringsSep "" [ "{" ( builtins.concatStringsSep "" track.reduced  ) "}" ] ;
-			      string = track : track.reduced ;
-			      undefined = track : builtins.throw "abccc82c-b517-4476-bb5a-2d9682b425a0" ;
-			      in _utils.visit { int = int ; list = list ; string = string ; undefined = undefined ; } ;
-			  in devShell ;
+                        let
+                          check =
+                            let
+                              lambda =
+                                track :
+                                  let
+                                    tester =
+                                      observer : success : value :
+                                      in
+                                        expected = { success = success ; value = value ; } ;
+                                        observed = builtins.tryEval ( observer implementation ) ;
+                                        to-string =
+                                          let
+                                            int = track : builtins.concatStrings "" [ "[" ( builtins.toString track.reduced ) "]" ] ;
+                                            list = track : builtins.concatStrings "," track.reduced ;
+                                            string = track : builtins.concatStrings "" [ "{" track.reduced "}" ] ;
+                                            undefined = track : builtins.throw "b6e1f9d2-4aee-45c1-83a8-cefd78d3f04b" ;
+                                    in if observed == expected then "" else to-string track.path ;
+                              list = track : builtins.concatStringsSep "," track.reduced ;
+                              set = track : builtins.concatStringsSep "," ( builtins.attrValues track.reduced ) ;
+                              in _utils.visit { lambda = lambda ; list = list ; set = set ; undefined = undefined ; } test '
+                          in { devShell = pkgs.mkShell { buildInputs = [ ( pkgs.writeShellScriptBin "check" "${ pkgs.coreutils }/bin/echo ${ check }" ) ] ; } ; }
 		  }
 	  ) ;
   }
